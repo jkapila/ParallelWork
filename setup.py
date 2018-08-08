96840ce29810d0b180d0551b2f319043c8b284a2
Jitin Kapila <jitin.kapila@gmail.com>
2018-08-08 15:02:01 +0530

    Added GNU License

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author = "Jitin Kapila",
     author_email = "jitin.kapila@gmail.com",
     packages=['parallelwork', ],
-    license="BSD",
+    license="GNU General Public License v3.0",
     description='A library for easy parallel processing!',
     url='http://github.com/jkapila/ParallelWork',
     zip_safe=False)
